458556f15d34583ae42165b350c95f80f912c0a1
denya <denya568@yandex.ru>
2018-04-20 23:35:10 +0300

    1.1

---
+++ app/src/main/java/wt23/ru/operator23/InetWork.java
@@ -0,0 +1,124 @@
+package wt23.ru.operator23;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class InetWork {
+    String adress = "https://wt23.ru/api/";
+
+    int size = 0;
+    private ArrayList<String> battle_id = new ArrayList<>();
+    private ArrayList<String> type = new ArrayList<>();
+    private ArrayList<String> category = new ArrayList<>();
+    private ArrayList<String> count_users = new ArrayList<>();
+    private ArrayList<String> date_start = new ArrayList<>();
+
+    public ArrayList<String> getBattle_id() {
+        return battle_id;
+    }
+
+    public ArrayList<String> getType() {
+        return type;
+    }
+
+    public ArrayList<String> getCategory() {
+        return category;
+    }
+
+    public ArrayList<String> getCount_users() {
+        return count_users;
+    }
+
+    public ArrayList<String> getDate_start() {
+        return date_start;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void getStreamBattles() {
+        getStreamBattlesJSON();
+    }
+
+
+
+
+
+
+    private void getStreamBattlesJSON(){
+        try {
+            URL url = new URL(adress + "get_stream_battles");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            //cert
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            TrustManager[] trustManagers = new TrustManager[] {
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
+                    }
+            };
+            HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return s.equals(sslSession.getPeerHost());
+                }
+            };
+            conn.setHostnameVerifier(hostnameVerifier);
+            sslContext.init(null, trustManagers, null);
+            conn.setSSLSocketFactory(sslContext.getSocketFactory());
+            //cert
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(50000);
+            conn.connect();
+
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(in);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            String text = sb.toString();
+
+            JSONArray dataJSON = new JSONArray(text);
+            for (int i = 0; i < dataJSON.length(); i++) {
+                JSONObject columns = dataJSON.getJSONObject(i);
+
+                this.battle_id.add(columns.getString("battle_id"));
+                this.type.add(columns.getString("type"));
+                this.category.add(columns.getString("category"));
+                this.count_users.add(columns.getString("count_users"));
+                this.date_start.add(columns.getString("date_start"));
+
+            }
+            this.size = dataJSON.length();
+
+            conn.disconnect();
+            br.close();
+            in.close();
+        } catch (Exception e) {
+            this.size = -1;
+        }
+    }
+
+
+}
--- app/src/main/java/wt23/ru/operator23/MainActivity.java
+++ app/src/main/java/wt23/ru/operator23/MainActivity.java
@@ -2,10 +2,23 @@ package wt23.ru.operator23;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout lay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +26,104 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        Intent intent = new Intent(MainActivity.this, StreamActivity.class);
-        startActivity(intent);
+        lay = (LinearLayout) findViewById(R.id.mainLay);
+
+    }
+
+    private class LoadBattlesAsync extends AsyncTask<Void, Void, Void> {
+        int size;
+        ArrayList<String> battle_id = new ArrayList<>();
+        ArrayList<String> type = new ArrayList<>();
+        ArrayList<String> category = new ArrayList<>();
+        ArrayList<String> count_users = new ArrayList<>();
+        ArrayList<String> date_start = new ArrayList<>();
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            InetWork inetWork = new InetWork();
+            inetWork.getStreamBattles();
+            size = inetWork.getSize();
+            if (size == -1) {
+
+            } else {
+                battle_id = inetWork.getBattle_id();
+                type = inetWork.getType();
+                category = inetWork.getCategory();
+                count_users = inetWork.getCount_users();
+                date_start = inetWork.getDate_start();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            if (size != -1) {
+
+                for (int i = 0; i < size; i++) {
+                    final int finalI = i;
+                    TableLayout.LayoutParams lp = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    lp.setMargins(10, 10, 10, 10);
+                    TableLayout tableLayout = new TableLayout(getApplicationContext());
+                    tableLayout.setOrientation(LinearLayout.VERTICAL);
+                    tableLayout.setLayoutParams(lp);
+
+                    TableRow tableRow = new TableRow(getApplicationContext());
+                    tableRow.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                    for (int j = 0; j < 5; j++) {
+                        TextView tvType = new TextView(getApplicationContext());
+                        tvType.setTextColor(Color.BLACK);
+                        tvType.setText(type.get(i));
+
+                        TextView tvCategory = new TextView(getApplicationContext());
+                        tvCategory.setTextColor(Color.BLACK);
+                        tvCategory.setText(category.get(i));
+
+                        TextView tvCount = new TextView(getApplicationContext());
+                        tvCount.setTextColor(Color.BLACK);
+                        tvCount.setText(count_users.get(i));
+
+                        TextView tvDate = new TextView(getApplicationContext());
+                        tvDate.setTextColor(Color.BLACK);
+                        tvDate.setText(date_start.get(i));
+
+                        Button start = new Button(getApplicationContext());
+                        start.setTextColor(Color.RED);
+                        start.setText("Start");
+                        start.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent openStream = new Intent(MainActivity.this, StreamActivity.class);
+                                openStream.putExtra("battle_id", battle_id.get(finalI));
+                            }
+                        });
 
 
+                    }
+
+
+                }
+
+
+            } else {
+                Toast.makeText(getApplicationContext(), "No network", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
     }
 
 
